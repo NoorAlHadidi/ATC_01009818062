1d5b92d0c385df56eff20e91f26f64e8acf973f0
Noor AlHadidi <noor.ahmed.alhadidi@gmail.com>
2025-05-10 01:30:38 +0300

    chore: initialised backend folder with nest modules, services and controllers

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
+import { ProfilesModule } from './profiles/profiles.module';
+import { AuthModule } from './auth/auth.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { BookingController } from './booking/booking.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [EventsModule, ProfilesModule, AuthModule, BookingsModule],
+  controllers: [AppController, BookingController],
   providers: [AppService],
 })
 export class AppModule {}
